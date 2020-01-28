5e297bc2394950766f57b97fc663f9b8003319d5
deksden <deksden@deksden.com>
2020-01-28 20:56:30 +0700

    feat: initial app based on cloud-deploy

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import appBuilder from './packages/app-builder'
+import serverBuilder from './packages/server-builder'
+import { Mrp } from './ext/mrp'
+import env from 'dotenv-safe'
+
+// load .env
+
+env.config()
+
+let app = null
+
+// build app & server
+appBuilder(express, {})
+  .then((_app) => {
+    app = _app
+    Mrp(app)
+  })
+  .then(() => app.exModular.storages.Init()) // init storages
+  .then(() => app.exModular.modelsInit())
+  .then(() => {
+    app.exModular.routes.builder.forAllModels()
+    return app.exModular.routes.builder.generateRoutes()
+  })
+  .then(() => app.exModular.initAll())
+  .then(() => serverBuilder(app, {}))
+  .catch((e) => { throw e })
