0c10c12826fa34d4b7fe261211bd39ce8e7f0851
deksden <deksden@deksden.com>
2020-10-01 09:34:25 +0700

    fix(app): remove unused module

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import appBuilder from './packages/app-builder'
 import serverBuilder from './packages/server-builder'
 import { Mrp } from './ext-mrp/mrp'
-import { ExtTest } from './ext-test/ext-test'
+// import { ExtTest } from './ext-test/ext-test'
 import env from 'dotenv-safe'
 
 // load .env
@@ -16,7 +16,7 @@ appBuilder(express, {})
   .then((_app) => {
     app = _app
     Mrp(app)
-    ExtTest(app)
+    // ExtTest(app)
   })
   .then(() => app.exModular.storages.Init()) // init storages
   .then(() => app.exModular.modelsInit())
